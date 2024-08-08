b15ae00c141e4d1861a55e6e570a4b95d376b5c6
AlviraKhan1505 <93580272+AlviraKhan1505@users.noreply.github.com>
2024-08-08 06:55:06 -0400

    Firebase Storage SDK

--- S2G6-CapstoneProject-INFO6134-master/build.gradle.kts
+++ S2G6-CapstoneProject-INFO6134-master/build.gradle.kts
@@ -11,6 +11,7 @@ plugins {
     id ("com.android.library") version "8.2.2" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
     id("org.jetbrains.kotlin.android") version "1.6.10" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
